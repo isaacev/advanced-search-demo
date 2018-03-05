11a9e63f7297696208ed1e1b1be2a66d838ea09d
isaacev <isaacevavold@gmail.com>
2018-03-04 19:40:55 -0600

    major refactor of query guess generator

--- src/lang/oracle.ts
+++ src/lang/oracle.ts
@@ -4,9 +4,11 @@ import Lexer, { RichToken } from './lexer'
 
 class FilterGuess {
   public name : string
+  public type : Type
 
-  constructor (name: string) {
+  constructor (name: string, type: Type) {
     this.name = name
+    this.type = type
   }
 }
 
@@ -120,8 +122,8 @@ export class Guess {
  * and semantically valid completions to the current query.
  *
  *
- * For example, given the incomplete query "created bef" good suggestions might
- * include:
+ * For example, given the incomplete query "created before" good suggestions
+ * might include:
  *
  * - created before <timestamp>
  * - created before "today"
@@ -156,90 +158,166 @@ export default class Oracle {
     return guesses
   }
 
-  // TODO: good grief this function needs to be broken down.
   private static filter (lexer: Lexer, grammar: Grammar): Guess[] {
-    const advice = [] as Guess[]
     if (lexer.peek() === null) {
       /**
-       * The language expected the start of a filter but instead found the end
-       * of the query. In this case offer completions representing all possible
-       * filters.
+       * The language expected the beginning of a filter but instead found an
+       * empty line. Since the user has input nothing that can be used to limit
+       * the breadth of guesses, offer 1 guess for every filter.
        */
-      grammar.filters.forEach(f => {
-        const filter = new FilterGuess(f.name)
+      return grammar.filters.map(f => {
+        const filter = new FilterGuess(f.name, f.type)
         const operator = new OperatorGuess(true)
         const argument = new ArgumentGuess(true, f.type)
-        advice.push(new Guess(filter, operator, argument))
+        return new Guess(filter, operator, argument)
       })
-    } else {
-      const next = lexer.next() as RichToken
-      grammar.filters
-        .filter(f => f.prefixedBy(next.lexeme))
-        .forEach((f, _, all) => {
-          const filter = new FilterGuess(f.name)
-
-          if (all.length > 1) {
-            // Since the filter is still ambiguous, do not expand the operators.
-            const operator = new OperatorGuess(true)
-            const argument = new ArgumentGuess(true, f.type)
-            advice.push(new Guess(filter, operator, argument))
-            return
-          }
-
-          if (lexer.peek() === null) {
-            // Nothing more in the query to parse.
-            grammar.operators
-              .filter(op => op.type.equals(f.type)) // TODO: possible do type casting?
-              .forEach(op => {
-                const operator = new OperatorGuess(false, op.symbol)
-                const argument = new ArgumentGuess(true, f.type)
-                advice.push(new Guess(filter, operator, argument))
-              })
-          } else {
-            // Parse the operator and then parse the argument.
-            const next = lexer.next() as RichToken
-            grammar.operators
-              .filter(op => op.type.equals(f.type)) // TODO: possible do type casting?
-              .filter(op => op.prefixedBy(next.lexeme))
-              .forEach(op => {
-                const operator = new OperatorGuess(false, op.symbol)
-
-                // Macros that return the same type as the filter.
-                const worthyMacros = grammar.macros
-                  .filter(macro => macro.type === f.type)
-
-                if (lexer.peek() === null) {
-                  // If there are not more tokens, suggest all possible macros.
-                  worthyMacros
-                    .forEach(macro => {
-                      const argument = new ArgumentGuess(false, f.type, macro.example([]), 0)
-                      advice.push(new Guess(filter, operator, argument))
-                    })
-                } else {
-                  // If there are more tokens, attempt each macro then generate
-                  // an example using existing tokens as much as possible.
-                  worthyMacros
-                    .map(macro => {
-                      lexer.save()
-                      const attempt = macro.attempt(lexer, grammar)
-                      lexer.undo()
-                      return { attempt, macro }
-                    })
-                    .forEach(pair => {
-                      const { attempt, macro } = pair
-                      const example = macro.example(attempt.tokens.map(t => t.lexeme))
-                      const weight = attempt.tokens.reduce((weight, tok) => {
-                        return weight + tok.lexeme.length
-                      }, 0)
-                      const argument = new ArgumentGuess(false, f.type, example, weight)
-                      advice.push(new Guess(filter, operator, argument))
-                    })
-                }
-              })
-            }
-        })
     }
 
-    return advice
+    const first = lexer.next() as RichToken
+
+    /**
+     * If the first token is the only token, identify all filters prefixed by
+     * the first token. If there is only one filter that is prefixed by the
+     * token then expand that filter's operators. If more than one filter is
+     * still possible, do not expand any filter operators.
+     */
+    if (lexer.peek() === null) {
+      const possibleFilters = grammar.filters.filter(f => {
+        return f.prefixedBy(first.lexeme)
+      }).map(f => {
+        return new FilterGuess(f.name, f.type)
+      })
+
+      if (possibleFilters.length === 1) {
+        const filter = possibleFilters[0]
+        return grammar.compatibleOperators(filter.type).map(op => {
+          const operator = new OperatorGuess(false, op.symbol)
+          const argument = new ArgumentGuess(true, filter.type)
+          return new Guess(filter, operator, argument)
+        })
+      } else {
+        return possibleFilters.map(filter => {
+          const operator = new OperatorGuess(true)
+          const argument = new ArgumentGuess(true, filter.type)
+          return new Guess(filter, operator, argument)
+        })
+      }
+    }
+
+    const second = lexer.next() as RichToken
+
+    /**
+     * If there is a second token then the only filters that can be considered
+     * are the filters that perfectly matched the first token, not filters that
+     * were merely prefixed by the first token.
+     */
+    const matchingFilters = grammar.filters.filter(f => {
+      return f.name === first.lexeme
+    }).map(f => {
+      return new FilterGuess(f.name, f.type)
+    })
+
+    /**
+     * If the second token is the last token, identify any operators compatible
+     * with the matching filters and that are prefixed by the second token. If
+     * there is only one operator that fits all of these criteria, expand the
+     * arguments.
+     */
+    if (lexer.peek() === null) {
+      return matchingFilters.reduce((guesses, filter) => {
+        const possibleOperators = grammar.compatibleOperators(filter.type).filter(op => {
+          return op.prefixedBy(second.lexeme)
+        }).map(op => {
+          return new OperatorGuess(false, op.symbol)
+        })
+
+        if (possibleOperators.length === 1) {
+          const operator = possibleOperators[0]
+          const examples = Oracle.argument(filter.type, lexer, grammar)
+
+          const newGuesses = examples.map(argument => {
+            return new Guess(filter, operator, argument)
+          })
+
+          return guesses.concat(newGuesses)
+        } else {
+          const newGuesses = possibleOperators.map(operator => {
+            const argument = new ArgumentGuess(true, filter.type)
+            return new Guess(filter, operator, argument)
+          })
+
+          return guesses.concat(newGuesses)
+        }
+      }, [] as Guess[])
+    }
+
+    return matchingFilters.reduce((guesses, filter) => {
+      /**
+       * Since there are still more tokens, the only operators that can be
+       * considered are the operators that perfectly matched the second token,
+       * not operators that were merely prefixed by the second token.
+       */
+      const matchingOperators = grammar.compatibleOperators(filter.type).filter(op => {
+        return op.hasAlias(second.lexeme)
+      }).map(op => {
+        return new OperatorGuess(false, op.symbol, )
+      })
+
+      /**
+       * Check each compatible macro to determine if it matches the upcoming
+       * tokens and if so, count how many characters it matches. If the macro
+       * matches none of the upcoming characters do not add it to the list of
+       * guesses.
+       *
+       * If the macro matches some characters, generate an example for that
+       * macro and add that result to the list of guesses.
+       */
+      const examples = Oracle.argument(filter.type, lexer, grammar)
+      const newGuesses = examples.reduce((guesses, argument) => {
+        const newGuesses = matchingOperators.map(operator => {
+          return new Guess(filter, operator, argument)
+        })
+
+        return guesses.concat(newGuesses)
+      }, [] as Guess[])
+
+      return guesses.concat(newGuesses)
+    }, [] as Guess[])
+  }
+
+  private static argument (type: Type, lexer: Lexer, grammar: Grammar): ArgumentGuess[] {
+    return grammar.compatibleMacros(type).map(m => {
+      /**
+       * If there are no upcoming tokens, every compatible macro should return
+       * an example.
+       */
+      if (lexer.peek() === null) {
+        const example = m.example([])
+        return new ArgumentGuess(false, type, example, 0)
+      }
+
+      /**
+       * Try to apply the macro to the token stream but undo any changes that
+       * parsing the macro made to the lexer.
+       */
+      lexer.save()
+      const attempt = m.attempt(lexer, grammar)
+      lexer.undo()
+
+      // Macro matched nothing so reject it as a possibility.
+      if (attempt.success === false && attempt.partial === false) {
+        return null
+      }
+
+      const example = m.example(attempt.tokens.map(t => t.lexeme))
+      const weight = attempt.tokens.reduce((w, t) => w + t.lexeme.length, 0)
+      return new ArgumentGuess(false, type, example, weight)
+    }).filter(argument => {
+      if (argument === null) {
+        return false
+      } else {
+        return true
+      }
+    }) as ArgumentGuess[]
   }
 }
